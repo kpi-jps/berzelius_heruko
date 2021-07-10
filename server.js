3e3069f77fdab92bf2870af9c2ced71dd7bfbe97
kpi-jps <jps.spj@gmail.com>
2021-07-10 01:37:30 -0300

    app url

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sc3012964-berzelius'));
+app.use(express.static('./sc3012964-berzelius'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sc3012964-berzelius/'}),
+    res.sendFile('index.html', {root: 'sc3012964-berzelius/'}),
 );
 
 // Start the app by listening on the default Heroku port
