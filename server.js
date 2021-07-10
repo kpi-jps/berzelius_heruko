e0262a91bf681e5ad315d49fd577945341626ebc
kpi-jps <jps.spj@gmail.com>
2021-07-10 01:40:41 -0300

    app url

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./sc3012964-berzelius'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + "/dist/sc3012964-berzelius"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'sc3012964-berzelius/'}),
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/sc3012964-berzelius/index.html"));
+});
 
-// Start the app by listening on the default Heroku port
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
